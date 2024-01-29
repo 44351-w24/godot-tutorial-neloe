c4e7b37a6b8fa2a30a816d6e810374c1c5df919a
Nathan Eloe <powerofazure@gmail.com>
2024-01-29 10:48:20 -0600

    Complete through HUD tutorial

--- main.gd
+++ main.gd
@@ -17,15 +17,21 @@ func _process(delta):
 func game_over():
 	$ScoreTimer.stop()
 	$MobTimer.stop()
+	
+	$HUD.show_game_over()
 
 func new_game():
 	score = 0
+	get_tree().call_group("mobs", "queue_free")
+	$HUD.update_score(score)
+	$HUD.show_message("Get Ready")
 	$Player.start($StartPosition.position)
 	$StartTimer.start()
 
 
 func _on_score_timer_timeout():
 	score += 1
+	$HUD.update_score(score)
 
 
 func _on_start_timer_timeout():
